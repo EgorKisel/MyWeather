6200ad736d15183c161ded4e9c59309860d3509d
Egor Kisel <kisel1989@mail.ru>
2022-04-08 14:29:16 +0300

    Добавил кнопку в разметку и повесил на неё clickListener в Activity (переход во вторую активити)

--- app/src/main/java/com/geekbrains/myweather/MainActivity.kt
+++ app/src/main/java/com/geekbrains/myweather/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.geekbrains.myweather
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.press_me)
+        button.setOnClickListener {
+            val intent = Intent(this@MainActivity, First::class.java)
+            intent.putExtra("key", "Kotlin")
+            startActivity(intent)
+        }
     }
+
 }
+
+
+
